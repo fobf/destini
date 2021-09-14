a60947427a5737fc12e716a21e911da0a9341aa1
Dmitriy K <fobfreeman@gmail.com>
2021-09-14 22:25:38 +0300

    update nextStory()

--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -37,18 +37,26 @@ class StoryBrain {
   String getStory() => _storyData.first.storyTitle;
   String getChoice1() => _storyData.first.choice1;
   String getChoice2() => _storyData.first.choice2;
-  void nextStory(int choiceNumber) {}
+  void nextStory(int userChoice) {
+    if (userChoice == 1 && storyNumber == 0) {
+      storyNumber = 2;
+    } else if (userChoice == 2 && storyNumber == 0) {
+      storyNumber = 1;
+    } else if (userChoice == 1 && storyNumber == 2) {
+      storyNumber = 5;
+    } else if (userChoice == 2 && storyNumber == 2) {
+      storyNumber = 4;
+    } else if (userChoice == 1 && storyNumber == 1) {
+      storyNumber = 2;
+    } else if (userChoice == 2 && storyNumber == 1) {
+      storyNumber = 3;
+    }
+  }
 }
 
 //TODO: Step 23 - Use the storyNumber property inside getStory(), getChoice1() and getChoice2() so that it gets the updated story and choices rather than always just the first (0th) one.
 
 //TODO: Step 25 - Change the storyNumber property into a private property so that only story_brain.dart has access to it. You can do this by right clicking on the name (storyNumber) and selecting Refactor -> Rename to make the change across all the places where it's used.
-
-//TODO: Step 17 - Create a method called nextStory(), it should not have any outputs but it should have 1 input called choiceNumber which will be the choice number (int) made by the user.
-
-//TODO: Step 20 - Download the story plan here: https://drive.google.com/uc?export=download&id=1KU6EghkO9Hf2hRM0756xFHgNaZyGCou3
-
-//TODO: Step 21 - Using the story plan, update nextStory() to change the storyNumber depending on the choice made by the user. e.g. if choiceNumber was equal to 1 and the storyNumber is 0, the storyNumber should become 2.
 
 //TODO: Step 22 - In nextStory() if the storyNumber is equal to 3 or 4 or 5, that means it's the end of the game and it should call a method called restart() that resets the storyNumber to 0.
 
