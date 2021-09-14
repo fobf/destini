fb5854442849521794136af409346c1aebcb9b47
Dmitriy K <fobfreeman@gmail.com>
2021-09-14 23:10:32 +0300

    +visibility,make func for button :visible or not

--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -40,6 +40,14 @@ class StoryBrain {
 
   void restart() => _storyNumber = 0;
 
+  bool buttonShouldBeVisible() {
+    if (_storyNumber == 0 || _storyNumber == 1 || _storyNumber == 2) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
   void nextStory(int userChoice) {
     if (userChoice == 1 && _storyNumber == 0) {
       _storyNumber = 2;
@@ -58,8 +66,3 @@ class StoryBrain {
     }
   }
 }
-
-
-//TODO: Step 25 - Change the _storyNumber property into a private property so that only story_brain.dart has access to it. You can do this by right clicking on the name (_storyNumber) and selecting Refactor -> Rename to make the change across all the places where it's used.
-
-//TODO: Step 27 - Create a method called buttonShouldBeVisible() which checks to see if _storyNumber is 0 or 1 or 2 (when both buttons should show choices) and return true if that is the case, else it should return false.
