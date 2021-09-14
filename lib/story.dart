41b66cb0190b67cb8a85374856e2dd9eb2c2af2e
Dmitriy K <fobfreeman@gmail.com>
2021-09-14 15:50:11 +0300

    storybrain and storydata

--- lib/story.dart
+++ lib/story.dart
@@ -2,5 +2,6 @@ class Story {
   String storyTitle;
   String choice1;
   String choice2;
-  Story(this.storyTitle, this.choice1, this.choice2);
+  Story(
+      {required this.storyTitle, required this.choice1, required this.choice2});
 }
